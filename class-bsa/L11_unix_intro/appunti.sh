f1f33fd7d8251416e9eebf9b9384bb2af18ea0b3
ChiaraSabina <chiara.sabina01@universitadipavia.it>
2024-11-06 16:59:26 +0000

    appunti

---
+++ class-bsa/L11_unix_intro/appunti.sh
@@ -0,0 +1,3 @@
+cd --> comando per spostarsi dalla cartella, cambiare cartella
+..  --> cartella sopra 
+- p --> se la cartella esiste non da errore, se non esiste la crea, se le diamo un intero percorso da crearare crea le cartelle
